7b18068ed0992d8c72a7241d732539d18c3219bd
Deng.Zixu <admin@dengzixu.com>
2021-07-26 11:31:48 +0800

    创建项目框架

---
+++ src/main/java/net/dengzixu/danmumu/DanmumuApplication.java
@@ -0,0 +1,13 @@
+package net.dengzixu.danmumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanmumuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanmumuApplication.class, args);
+	}
+
+}
